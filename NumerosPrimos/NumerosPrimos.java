d9da67dbd95ea405d576d54e8db4c9c2e74a1711
Gabriel Brasileiro <gah.lucas@gmail.com>
2017-09-11 23:40:46 -0300

    Add files via upload

---
+++ NumerosPrimos/NumerosPrimos.java
@@ -0,0 +1,43 @@
+package NumerosPrimos;
+
+public class NumerosPrimos {
+	
+	static int total= 1000;
+	
+	public static void main(String[] args) {
+		
+		int v[] = new int[total];
+		
+		for(int i = 2; i < total; i++) {
+			
+			v[i] = i;
+			
+		}
+		
+		for(int i = 2; i < total; i++) {
+			
+			if(v[i] != 0) {
+				
+				for(int j = 2 * i; j < total; j += i) {
+					
+					v[j] = 0;
+					
+				}
+				
+			}
+			
+		}
+		
+		for(int i = 2; i < total; i++) {
+			
+			if(v[i] != 0) {
+				
+				System.out.print(v[i] + "-");
+				
+			}
+			
+		}
+		
+	}
+
+}
